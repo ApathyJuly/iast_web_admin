ce68f17677d089052bcf0a9fce88d1c4d6cd0ca7
Apathyjuly <2897250580@qq.com>
2023-04-13 23:18:35 +0800

    补充代码

--- src/views/iast/index.vue
+++ src/views/iast/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="app-container">
-      
+      组件
     </div>
   </template>
   
@@ -8,16 +8,10 @@
   export default {
     data() {
       return {
-        
       }
     },
     methods: {
-     
     }
   }
   </script>
-  
-  <style scoped>
-  </style>
-  
   
