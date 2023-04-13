71b17f192dc82a99b07d5cd88995d1d6cf132e9e
Apathyjuly <2897250580@qq.com>
2023-04-13 23:50:29 +0800

    https://github.com/emedware/vue-golden-layout  安装

--- src/views/iast/index.vue
+++ src/views/iast/index.vue
@@ -1,10 +1,25 @@
 <template>
     <div class="app-container">
-      组件
+      <golden-layout>
+  <gl-row>
+    <gl-component title="component1">
+      <h1>Component 1</h1>
+    </gl-component>
+    <gl-stack>
+      <gl-component title="component2">
+        <h1>Component 2</h1>
+      </gl-component>
+      <gl-component title="component3">
+        <h1>Component 3</h1>
+      </gl-component>
+    </gl-stack>
+  </gl-row>
+</golden-layout>
     </div>
   </template>
-  
+
   <script>
+
   export default {
     data() {
       return {
